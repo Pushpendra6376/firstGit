ca99c4a4150339f3401be1c7b6601c610c68a589
pushpendra6376 <pp5395021@gmail.com>
2024-12-22 21:51:14 +0530

    Added DOM_manipulation_Assignment1 folder

---
+++ DOM_manipulation_Assignment1/DOM_Manpulation.js
@@ -0,0 +1,16 @@
+// Write your code below:
+// Write your code below:
+const mainHeading = document.getElementById("main-heading");
+mainHeading.textContent = "Fruit World";
+mainHeading.style.color = "orange";
+
+
+const newHeader = document.getElementById("header");
+newHeader.style.backgroundColor = "green";
+newHeader.style.borderBottom = " 3px solid orange";
+
+const fruitsInbasket = document.getElementById("basket-heading");
+fruitsInbasket.style.color = "green";
+
+const thanks = document.getElementById("thanks");
+thanks.innerHTML = "<p>Please visit us again</p>"
